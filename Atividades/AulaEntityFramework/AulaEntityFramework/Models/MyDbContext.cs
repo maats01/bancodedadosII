b334fdb6dc810cae5a33d0452326354bc25bc9b0
maats01 <matheusp.sampaio@gmail.com>
2024-09-06 11:14:00 -0300

    foreign key

--- Atividades/AulaEntityFramework/AulaEntityFramework/Models/MyDbContext.cs
+++ Atividades/AulaEntityFramework/AulaEntityFramework/Models/MyDbContext.cs
@@ -17,5 +17,6 @@ namespace AulaEntityFramework.Models
         // em tabelas do BD
 
         public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
     }
 }
