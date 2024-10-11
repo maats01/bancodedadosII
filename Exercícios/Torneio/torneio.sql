0d9b02d45d7c96f61104d81dc266409ea39777d8
maats01 <matheusp.sampaio@gmail.com>
2024-10-10 21:18:10 -0300

    atualizando tabela torneio

--- Exercícios/Torneio/torneio.sql
+++ Exercícios/Torneio/torneio.sql
@@ -12,9 +12,14 @@ CREATE TABLE Equipe(
     nome VARCHAR(255)
 );
 
-CREATE TABLE Etapa(
+CREATE TABLE Chave(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    etapa VARCHAR(255)
+    chave VARCHAR(255)
+);
+
+CREATE TABLE Rodada(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    rodada VARCHAR(255)
 );
 
 CREATE TABLE Modalidade(
@@ -25,42 +30,48 @@ CREATE TABLE Modalidade(
 CREATE TABLE Torneio(
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nome varchar(255),
-    equipeVencedoraId INT NOT NULL,
-    dataInicio Datetime,
-    dataFim Datetime,
-    FOREIGN KEY (equipeVencedoraId) REFERENCES Equipe(id)
+    equipe_vencedora_id INT NOT NULL,
+    data_inicio DATETIME,
+    data_fim DATETIME,
+    FOREIGN KEY (equipe_vencedora_id) REFERENCES Equipe(id)
 );
 
-CREATE TABLE Classificatoria(
-	torneioId INT NOT NULL,
-    equipeId INT NOT NULL,
+CREATE TABLE EquipeChaveTorneio(
+	torneio_id INT NOT NULL,
+    chave_atual_id INT NOT NULL,
+    chave_anterior_id INT,
+    equipe_id INT NOT NULL,
     pontos INT DEFAULT(0),
-    FOREIGN KEY (torneioId) REFERENCES Torneio(id),
-    FOREIGN KEY (equipeId) REFERENCES Equipe(id)
+    FOREIGN KEY (torneio_id) REFERENCES Torneio(id),
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(id),
+    FOREIGN KEY (chave_atual_id) REFERENCES Chave(id),
+    FOREIGN KEY (chave_anterior_id) REFERENCES Chave(id)
 );
 
 CREATE TABLE EquipeAtleta(
-	atletaId INT NOT NULL,
-    equipeId INT NOT NULL,
-    FOREIGN KEY (atletaId) REFERENCES Atleta(id),
-    FOREIGN KEY (equipeId) REFERENCES Equipe(id)
+	atleta_id INT NOT NULL,
+    equipe_id INT NOT NULL,
+    FOREIGN KEY (atleta_id) REFERENCES Atleta(id),
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(id)
 );
 
 CREATE TABLE Partida(
 	id INT AUTO_INCREMENT PRIMARY KEY,
-    torneioId INT NOT NULL,
-    etapaId INT NOT NULL,
-    numRodada INT,
-    equipe1 INT NOT NULL,
-    equipe2 INT,
-    modalidadeId INT NOT NULL,
-    vencedorId INT DEFAULT NULL,
+    torneio_id INT NOT NULL,
+    rodada_id INT NOT NULL,
+    modalidade_id INT NOT NULL,
     empate BOOL DEFAULT(FALSE),
-    dataPartida DATETIME NOT NULL,
-    FOREIGN KEY (torneioId) REFERENCES Torneio(id),
-    FOREIGN KEY (etapaId) REFERENCES Etapa(id),
-    FOREIGN KEY (equipe1) REFERENCES Equipe(id),
-    FOREIGN KEY (equipe2) REFERENCES Equipe(id),
-    FOREIGN KEY (modalidadeId) REFERENCES Modalidade(id),
-    FOREIGN KEY (vencedorId) REFERENCES Equipe(id)
+    data_partida DATETIME NOT NULL,
+    FOREIGN KEY (torneio_id) REFERENCES Torneio(id),
+    FOREIGN KEY (rodada_id) REFERENCES Rodada(id),
+    FOREIGN KEY (modalidade_id) REFERENCES Modalidade(id)
+);
+
+CREATE TABLE PartidaEquipe(
+	partida_id INT NOT NULL,
+    equipe_id INT NOT NULL,
+    pontos INT DEFAULT(0),
+    vencedor BOOL DEFAULT(FALSE),
+    FOREIGN KEY (partida_id) REFERENCES Partida(id),
+    FOREIGN KEY (equipe_id) REFERENCES Equipe(id)
 );
